de041ad4a947e31ce6916f278879e9c8a0aaa615
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2022-11-17 04:29:09 +0000

    [Update]モデル内容変更

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,12 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_one_attached :profile_image
+  
+  def get_profile_image
+    unless prfile_image.attached?
+      file_path = Railes.root.join('app/assets/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
 end
