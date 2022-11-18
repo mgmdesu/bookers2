0d571f49d7b4e06c44d642bff7b0dcdf6b5ba736
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2022-11-18 07:19:33 +0000

    [Update]book/index

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,11 @@
 class Book < ApplicationRecord
   belongs_to :user
+  
   has_one_attached :profile_image
   
   def get_profile_image
     unless profile_image.attached?
-      file_path = Railes.root.join('app/assets/no_image.jpg')
+      file_path = Railes.root.join('app/assets/images_kadai3/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
